4ba01ce29ef8c10becd0745783f3991c73fd13cb
nodirbeknurqulov <nodirbeknurqulov096@gmail.com>
2022-05-16 17:23:45 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/appimpulseteamproject/AppImpulseTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.appimpulseteamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppImpulseTeamProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppImpulseTeamProjectApplication.class, args);
+    }
+
+}
